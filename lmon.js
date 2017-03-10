d87c8c37a244c097afe8d61ed5f3d478db763425
mjyuen <michelleyuen715@gmail.com>
2017-03-10 17:13:54 -0500

    newlmon.js attempt

--- lmon.js
+++ lmon.js
@@ -1,130 +1,153 @@
 function main()
 {
-  var d = new Date();
-  var day = d.getDay();
-  var hour = d.getHours();
-  var min = d.getMinutes();
+  var d = new Date(); // date object
+  var dayOfWeek = d.getDay(); // day of the week
+  var hour = d.getHours(); // hour of the day
+  var min = d.getMinutes(); // minute of the hour
 
-  var isLateMeal = true;
+  var isLateMeal = false;
+  var typeLateMeal = 0; // 0 for lunch, 1 for dinner
 
-  if (day == 0 || day == 6)
-    isLateMeal = false;
-  else if (day == 5)
+  // No late meal on the weekend
+  if (dayOfWeek != 0 && dayOfWeek != 6)
   {
-    if (hour < 14 || hour >= 16)
-      isLateMeal = false;
+    // Late lunch every Mon-Fri 2pm - 3:45pm
+    if (hour == 14 || (hour == 15 && min <= 45))
+    {
+      isLateMeal = true;
+      typeLateMeal = 0; // lunch
+    }
+    // late dinner every Mon-Thurs 8:30pm - 10pm
+    if (dayOfWeek != 5 &&
+      ((hour == 20 && min >= 30)|| (hour == 21) ||
+        (hour == 22 && min == 0)))
+    {
+      isLateMeal = true;
+      typeLateMeal = 1;
+    }
   }
-  else if (hour < 14 || hour >= 22)
-    isLateMeal = false;
-  else if (hour >= 16 && hour < 20)
-    isLateMeal = false;
-  else if (hour == 3 && min >= 45)
-    isLateMeal = false;
-  else if (hour == 8 && min < 30)
-    isLateMeal = false;
-  else if (hour == 15 && min > 45)
-  	isLateMeal = false;
-    
-  change(isLateMeal);
-  
+
+  changeText(isLateMeal); // update site text
+
+  // Show time until or left of late meal
   if (isLateMeal)
-    timeLeft(day, hour, min);
-  else
-    timeTill(day, hour, min);
-}
-
-function change(a)
-{
-  if (a)
-    {document.getElementById("yesJava").innerHTML = "Yes!";}
-  else
-    {document.getElementById("noJava").innerHTML = "Nah.";}
-}
-
-function timeLeft(day, hour, min)
-{
-  if (hour == 14)
   {
-    if (min > 45)
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + "1 hour and " + (min-45) + " minutes until Late Meal ends." + "&nbsp";
-    else
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + "1 hour and " + (105-min) + " minutes until Late Meal ends." + "&nbsp";
-  }
-  else if (hour == 15)
-  {
-    if (min > 45)
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + (105 - min) + " minutes until Late Meal ends." + "&nbsp";
-    else
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + "1 hour and " + (45-min) + " minutes until Late Meal ends." + "&nbsp";
-  }
-  else if (hour == 20)
-  {
-    document.getElementById("timeUntil").innerHTML = "&nbsp" + "1 hour and " + (60-min) + " minutes until Late Meal ends." + "&nbsp";
-  }
-  else if (hour == 21)
-  {
-    document.getElementById("timeUntil").innerHTML = "&nbsp" + (60-min) + " minutes until Late Meal ends." + "&nbsp";
-  }
-}
-
-function timeTill(day, hour, min)
-{
-  var hl;
-  var ml;
-  
-  if (day == 0 || day == 6)
-    document.getElementById("timeUntil").innerHTML = "&nbsp" + "No Late Meal on the weekend, silly." + "&nbsp";
-  else if (day == 5)
-  {
-    if (hour == 0)
-    {
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + "13 hours and " + (60-min) + " minutes until Late Meal begins." + "&nbsp";
-    }
-    else if (hour < 14)
-    {
-  	  hl = 14 - hour;
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + (hl-1) + " hours and " + (60-min) + " minutes until Late Meal begins." + "&nbsp";
-    }
-    else
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + min + "No more Late Meal today. Sorry, friend." + "&nbsp";
+    timeLeft(typeLateMeal, hour, min);
   }
   else
   {
-    if (hour == 0)
+    timeTill(dayOfWeek, hour, min);
+  }
+
+}
+// Update site text
+function changeText(isLateMeal)
+{
+  if (isLateMeal)
+  {
+    document.getElementById("yesJava").innerHTML = "Yes!";
+  }
+  else
+  {
+    document.getElementById("noJava").innerHTML = "Nah.";
+  }
+}
+// Calculate and display time left of late meal
+function timeLeft(typeLateMeal, hour, min)
+{
+  var hourLeft
+  var minLeft;
+
+  if (typeLateMeal == 0) // lunch late meal
+  {
+    hourLeft = 15 - hour;
+    minLeft = (45 + (60 * hourLeft)) - min;
+    if (minLeft > 45) // 2:45 - 3 window
     {
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + "13 hours and " + (60-min) + " minutes until Late Meal begins." + "&nbsp";
+      hourLeft = 0;
     }
-    else if (hour < 14)
+
+    if (hourLeft != 0)
     {
-  	  hl = 14 - hour;
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + (hl-1) + " hours and " + (60-min) + " minutes until Late Meal begins." + "&nbsp";
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + hourLeft + " hour and " + minLeft + " minutes until Late Meal ends." + "&nbsp";
     }
-    else if (hour == 20 && min < 30)
-    {
-      ml = 30 - min;
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + ml + " minutes until Late Meal begins." + "&nbsp";
-    }
-    else if ((hour < 20) && (min < 30))
-    {
-      hl = 20 - hour;
-      ml = 30 - min;
-      document.getElementById("timeUntil").innerHTML = "&nbsp" + hl + " hours and " + ml + " minutes until Late Meal begins." + "&nbsp";
-    } // make it work at 3:55
     else
+    {
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + minLeft + " minutes until Late Meal ends." + "&nbsp";
+    }
+  }
+  else // dinner late meal
+  {
+    hourLeft = 21 - hour; // hour remaining only if it's before 9
+    minLeft = 60 - min;
+
+    if (hourLeft != 0)
+    {
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + hourLeft + " hour and " + minLeft + " minutes until Late Meal ends." + "&nbsp";
+    }
+    else
+    {
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + minLeft + " minutes until Late Meal ends." + "&nbsp";
+    }
+  }
+}
+// Calculates and displays time until late meal starts
+function timeTill(dayOfWeek, hour, min)
+{
+  var hourLeft;
+  var minLeft;
+
+  // Weekend
+  if (dayOfWeek == 0 || dayOfWeek == 6)
+  {
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + "No Late Meal on the weekend, silly." + "&nbsp";
+  }
+  // Friday
+  else if (dayOfWeek == 5)
+  {
+    if (hour < 14) // before 2pm
+    {
+      hourLeft = 13 - hour;
+      minLeft = 60 - min;
+
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + hourLeft + " hours and " + minLeft + " minutes until Late Meal begins" + "&nbsp";
+    }
+    else // after late lunch
+    {
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + "No more Late Meal today. Sorry, friend." + "&nbsp"
+    }
+  }
+  else
+  {
+    if (hour < 14) // before 2pm
+    {
+      hourLeft = 13 - hour;
+      minLeft = 60 - min;
+
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + hourLeft + " hours and " + minLeft + " minutes until Late Meal begins" + "&nbsp";
+    }
+    else if (hour == 20 && min < 30) // before 8:30pm but after 8pm
+    {
+      minLeft = 30 - min;
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + minLeft + " minutes until Late Meal begins." + "&nbsp";
+    }
+    else if ((hour < 20) && (min <= 30))
+    {
+      hourLeft = 20 - hour;
+      minLeft = 30 - min;
+
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + hourLeft + " hours and " + minLeft + " minutes until Late Meal begins" + "&nbsp";
+    }
+    else if ((hour < 20) && (min > 30))
+    {
+      hourLeft = 19 - hour;
+      minLeft = 60 - min;
+
+      document.getElementById("timeUntil").innerHTML = "&nbsp" + hourLeft + " hours and " + minLeft + " minutes until Late Meal begins" + "&nbsp";
+    }
+    else
+    {
       document.getElementById("timeUntil").innerHTML = "&nbsp" + "No more Late Meal today. Sorry, friend." + "&nbsp";
-  }
+    }
+  } 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
